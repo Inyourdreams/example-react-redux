0140d883bcbd13eb36ba692da46f6fd63465e74c
Pavel Romanchenko <PRomanchenko@alfabank.ru>
2021-01-09 15:41:25 +0200

    feat(*): connect logic to customer component

--- src/client/pages/customers/customerLayout.js
+++ src/client/pages/customers/customerLayout.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import { useSelector } from "react-redux";
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from "react-redux";
 import { appViewSelector } from "../../selectors";
 import { currentCustomerInfoSelector } from "../../../store/models/customers/selectors";
 
 export const CustomerLayout = () => {
+    const dispatch = useDispatch();
+
+    // cелекторы нужных данных, вьюха не знает как мы их подготавливаем, просто отображает
     const isVisible = useSelector(appViewSelector);
     const currentCustomer = useSelector(currentCustomerInfoSelector);
+
+    // так же дёрагние контроллера, вьюха не знает ничего о контроллере и модели, она просто знает, что ей нужно среагировать
+    // на взаимодействие с пользователем
+    const deleteCustomer = useCallback((id) => {
+
+        // диспатчим экшн, который будет обработан в другом месте
+        dispatch(deleteCustomer(id));
+    }, [dispatch]);
 
     if (!isVisible) {
         return null;
@@ -14,6 +25,7 @@ export const CustomerLayout = () => {
     return (
         <div>
             { currentCustomer.id }
+            <button onClick={ () => deleteCustomer(currentCustomer.id) }> delete customer </button>
         </div>
     );
 }
